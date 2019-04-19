c978576369c06f84cbe19ef7f62e4830f11763f5
Daniel Hovorka <dhovorka10@gmail.com>
2019-04-18 19:22:35 -0500

    dan wins

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,5 @@
 class Api::PagesController < ApplicationController
   def index
-    render json: { message: "My name is Peter! Dan Hello! My name is Mike" }
+    render json: { message: "My name is Peter! Dan Hello! My name is Mike, no my name is actually Dan" }
   end
 end
